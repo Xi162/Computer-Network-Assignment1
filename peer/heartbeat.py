6bdc3a2c7a73be9c5df557c3e115c4e7a5559373
Tran Ngoc Dnag Khoa <tndangkhoa218@gmail.com>
2023-12-19 22:58:08 +0700

    fix: try catch

--- peer/heartbeat.py
+++ peer/heartbeat.py
@@ -5,6 +5,7 @@ import constants
 
 def send_heartbeat(connectSocket):
     while True:
+        try:
             req = {
                 "type": "heartbeat"
             }
@@ -12,4 +13,7 @@ def send_heartbeat(connectSocket):
             connectSocket.sendall(bytes(reqJSON, "utf8"))
             res = str(connectSocket.recv(1024), 'utf8')
             print(res)
+        except:
+            pass
+        finally:
             time.sleep(5.0)
